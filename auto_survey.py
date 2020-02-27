31ca6fabf3c6fa7a50fafb6cf29de63e593cb241
Jisoo Kim <sjw03151@naver.com>
2020-02-27 14:08:30 +0900

    Request contents of url

--- auto_survey.py
+++ auto_survey.py
@@ -2,3 +2,16 @@
     The full right of this code is on Jisoo Kim
     This project is to get special code after survey of burgerking
 '''
+#2020_02_07 //request test
+
+import urllib.request as req
+
+url = "http://www.postech.ac.kr"
+def request(url):
+    response = req.urlopen(url) #object
+    byte_data = response.read() #binary
+    text_data = byte_data.decode('utf-8') #text
+    return text_data
+
+#=========== print contents of url =============
+#print(request(url))
