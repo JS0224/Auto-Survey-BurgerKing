aa30806a3f0d0091e1488355e5106ec3962f682a
Jisoo Kim <sjw03151@naver.com>
2020-03-01 13:35:36 +0900

    Selenium input filling in 2nd page

--- auto_survey.py
+++ auto_survey.py
@@ -4,6 +4,7 @@
 '''
 #2020_02_07 //request test
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 url = "http://kor.tellburgerking.com"
 chrome_driver_path = "./chromedriver.exe"
@@ -12,6 +13,13 @@ chrome_driver_path = "./chromedriver.exe"
 chrome_browser = webdriver.Chrome(chrome_driver_path)
 chrome_browser.get(url)
 
+#####PAGE1 - Start!
 #get button by name
 next_btn = chrome_browser.find_element_by_name("NextButton")
 next_btn.submit()
+
+#####PAGE2 - Code typing
+code = ["821","948","010","037","113","2"]
+for i,v in enumerate(code):
+    name = "CN" + str(i+1)
+    cn = chrome_browser.find_element_by_name(name).send_keys(v)
