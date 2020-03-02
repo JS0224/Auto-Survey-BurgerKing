d67d1e28cb356d142f461fee5e0cf0ca77610043
Jisoo Kim <sjw03151@naver.com>
2020-03-02 17:04:28 +0900

    Question type summary

---
+++ question_data.py
@@ -0,0 +1,12 @@
+'''
+type0 - NextButton
+type1 - radioButtonHolder : click option in irrelevant options
+type2 - radioSimpleInput : click quality in related options
+type3 - customized or something else
+'''
+question_type=
+[
+ 0,3,1,1,1,2,2,2,2,3,
+ 3,2,0,3,3,2,2,2,2,2,
+ 1,1,1,3,0,3
+]
